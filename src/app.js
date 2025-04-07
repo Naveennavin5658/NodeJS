17c4fa894a0bd27c9aa23feef2dcc766886f0e2b
naveen <naveennavin5659@gmail.com>
2025-04-08 00:11:25 +0530

    explore GET POST in express hs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require("express"); // we are importing a C library lol!
 
 const app = express(); //we are creating an express JS app literally with a line of code!
+//Order of routing matters a lot!!!
+//app.use will match all http methods be it GET or POST or PUT or DELETE
+app.use("/health-check/3", (req, res) => {
+  res.send("Health check route 3 passed...");
+});
 
 app.use("/health-check", (req, res) => {
   res.send("Health check route passed...");
@@ -10,8 +15,14 @@ app.use("/health-check-test2", (req, res) => {
   res.send("Health check route path 2 passed...");
 });
 
-app.use((req, res) => {
-  res.send("Anonymus route passed...");
+// app.use((req, res) => {
+//   res.send("Anonymus route passed...");
+// });
+
+//First GET Request
+app.get("/users", (req, res) => {
+  console.log("Fetching all users idiot");
+  res.send("Sending all users details, wait you idiot");
 });
 
 app.listen(3000, () => {
