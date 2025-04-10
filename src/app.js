146441a0d3cb06ac8739ddd1126c7c0ad493497a
naveen <naveennavin5659@gmail.com>
2025-04-11 00:36:34 +0530

    Play with multiple route handlers in a single route

--- src/app.js
+++ src/app.js
@@ -3,38 +3,35 @@ const express = require("express"); // we are importing a C library lol!
 const app = express(); //we are creating an express JS app literally with a line of code!
 //Order of routing matters a lot!!!
 //app.use will match all http methods be it GET or POST or PUT or DELETE
-app.use("/health-check/3", (req, res) => {
-  res.send("Health check route 3 passed...");
+
+app.use("/user", (req, res) => {
+  //GET OR POST OR PUT OR DELETE
+  //route handlers
+  console.log("NAMASKARAM");
 });
 
-app.use("/health-check", (req, res) => {
-  res.send("Health check route passed...");
-});
-
-app.use("/health-check-test2", (req, res) => {
-  res.send("Health check route path 2 passed...");
-});
-
-app.use("/test-route", (req, res) => {
-  console.log(req.query);
-  res.send("Learning query obj controllers....", req.query);
-});
-
-app.use("/test-route-1/:id/:name/:pwd", (req, res) => {
-  console.log(req.params.name);
-  res.send("Learning query params access in controllers....", req.params);
-});
-
-// app.use((req, res) => {
-//   res.send("Anonymus route passed...");
-// });
-
-//First GET Request
-app.get("/users", (req, res) => {
-  console.log("Fetching all users idiot");
-  res.send("Sending all users details, wait you idiot");
-});
-
+// One route can have multiple route handlers!!
+// app.use("/endpoint",[routeHandler1,routeHandler2,routeHandler3,routeHandler4] using next param)
+//this can even be app.use("/endpoint",[routeHandler1,routeHandler2],routeHandler3,routeHandler4 using next param)
+app.use("/testuser", [
+  (a, b, next) => {
+    console.log("hi");
+    next();
+  },
+  (a, b, next) => {
+    console.log("Hello");
+    next();
+  },
+  (a, b, next) => {
+    console.log("Hello");
+    next();
+  },
+  (a, b) => {
+    console.log("Hello");
+    b.send("Namste");
+    // next();
+  },
+]);
 app.listen(3000, () => {
   console.log("Successfully listening on port 3000...");
 }); // we created a server and started to listen on port 3000
