3d3779f99850e1600e5d9c8b9924df1be6bb231a
naveen <naveennavin5659@gmail.com>
2025-05-02 17:52:27 +0530

    Handle dynamic data in signup api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,20 @@ const connectDB = require("./config/database");
 const app = express();
 
 const User = require("./models/user");
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  user_payload = req.body;
+
+  const userInstance = new User(user_payload);
+  try {
+    await userInstance.save(); //returns a promise
+    res.status(201).send({ message: "User record inserted successfully!" });
+  } catch (err) {
+    res.status(400).send({ message: "Something went wrong.." });
+  }
+});
 connectDB()
   .then(() => {
     console.log("DB connection successful");
@@ -13,22 +27,3 @@ connectDB()
   .catch((err) => {
     console.log("DB Connection failed due to error ", err);
   });
-
-app.post("/signup", async (req, res) => {
-  console.log("Started to insert user");
-  const userObj = {
-    firstName: "Naveen",
-    lastName: "Srikaram",
-    age: 24,
-    emailId: "naveennavin5659@gmail.com",
-    gender: "Male",
-    password: "sample",
-  };
-  const userInstance = new User(userObj);
-  try {
-    await userInstance.save(); //returns a promise
-    res.status(201).send({ message: "User record inserted successfully!" });
-  } catch (err) {
-    res.status(400).send({ message: "Something went wrong.." });
-  }
-});
